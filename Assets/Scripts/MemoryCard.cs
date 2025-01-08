79d7b183e10577d2b22791e43fa9e803e40d0478
MarianCroitor <croitor140@gmail.com>
2025-01-08 15:48:56 +0200

    flip animations

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using System.Collections;
 
 public class MemoryCard : MonoBehaviour
 {
     [SerializeField] GameObject cardBack;
-    private SceneController controller; 
+    private SceneController controller;
+    private Animator animator;
 
     private int _id;
     public int Id { get { return _id; } }
@@ -16,26 +18,33 @@ public class MemoryCard : MonoBehaviour
 
     private void Start()
     {
-        
         controller = Object.FindFirstObjectByType<SceneController>();
         if (controller == null)
         {
             Debug.LogError("SceneController not found in the scene!");
         }
+        animator = GetComponent<Animator>();
     }
-
 
     public void OnMouseDown()
     {
         if (controller != null && cardBack.activeSelf && controller.canReveal)
         {
             cardBack.SetActive(false);
+            animator.SetTrigger("Open");
             controller.CardRevealed(this);
         }
     }
 
     public void Unrevealed()
     {
+        animator.SetTrigger("Close"); 
+        StartCoroutine(ReenableCardBackAfterAnimation());
+    }
+
+    private IEnumerator ReenableCardBackAfterAnimation()
+    {
+        yield return new WaitForSeconds(0.25f);
         cardBack.SetActive(true);
     }
 }
